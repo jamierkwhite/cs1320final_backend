6a0b6022569323cc6d9e9c37c937dfd792f97df2
James White <james_white2@brown.edu>
2021-04-18 13:24:37 -0400

    added ability to add users

---
+++ app/new_user.py
@@ -0,0 +1,23 @@
+import argparse
+import os
+import psycopg2
+import hashlib
+
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+cursor = conn.cursor()
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--username', required=True)
+parser.add_argument('--password', required=True)
+args = parser.parse_args()
+
+username = args.username
+password = args.password
+
+hsh = hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+q = "INSERT INTO users VALUES(%s, %s)"
+cursor.execute(q, username, hsh)
+conn.commit()
+conn.close()
